49e267798b03e3ea518b647a6fcc08fbb3a2a9a9
fraciccio2 <francesco.giacomazzo2@gmail.com>
2024-11-14 14:42:31 +0100

    feat: update version angular 18

--- libs/home-feature/src/lib/home-feature.module.ts
+++ libs/home-feature/src/lib/home-feature.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HomeFeatureComponent } from './home-feature/home-feature.component';
 import { HomeUiModule } from '@beautify-json/home-ui';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeUiModule,
     RouterModule.forRoot([{ path: '', component: HomeFeatureComponent }]),
-    HttpClientModule,
   ],
   declarations: [HomeFeatureComponent],
   exports: [HomeFeatureComponent],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class HomeFeatureModule {}
